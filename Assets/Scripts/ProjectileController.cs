010e054a6c37119d3a4eb622e2d64fb3eacab832
lor1dok <47904961+lor1dok@users.noreply.github.com>
2023-04-19 22:21:09 +0300

    add prediction line feature and logic, add CalculatePathWithHeight function for future feature

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -1,53 +1,122 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ProjectileController : MonoBehaviour {
 
     [SerializeField] private Transform firePoint;
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private float lineStep;
 
-    private const float initSpeed = 10f;
+    private float initSpeed = 10f;
     private float initAngle = 45f;
+    private float projectileLifeTime = 5f;
 
-    private float projectileLifeTime = 5;
+    private Camera cameraMain;
+
+    private Vector3 startPos;
+
     private float currentLifeTime;
-
-    private Vector2 startPos;
+    private bool isOnFlight;
 
     private void Awake() {
-        currentLifeTime = 0;
-        initAngle *= Mathf.Deg2Rad;
-
-        MovementHandler(initSpeed, initAngle);
-
+        startPos = firePoint.position;
         Physics.gravity.Set(1f,1f,1f);
-
-        startPos = transform.position;
-
-        Debug.Log(startPos);
     }
 
+    private void Start() {
+        cameraMain = Camera.main;
+    }
 
-    private void Update() {
+    private void FixedUpdate() {
 
+        float initAngleRad = initAngle * Mathf.Deg2Rad;
+        Vector3 targetPos = cameraMain.ScreenToWorldPoint(Input.mousePosition) - startPos;
+
+        float v0;
+        float time;
+
+        CalculatePath(targetPos, initAngleRad, out v0, out time);
+
+        DrawPath(v0, initAngleRad, lineStep, time);
+        
         if (Input.GetKeyDown(KeyCode.Space)) {
             currentLifeTime = 0;
+            isOnFlight = true;
         }
 
-        MovementHandler(initSpeed, initAngle);
+        if (isOnFlight) {
+            MovementHandler(v0, initAngleRad, time);
+        }
+
     }
 
-    private void MovementHandler(float initSpeed, float initAngle) {
+    public void CalculatePath(Vector3 targetPos, float initAngle, out float v0, out float time) {
+        float x = targetPos.x;
+        float y = targetPos.y;
+        float g = -Physics.gravity.y;
 
-        if (currentLifeTime < projectileLifeTime) {
-            float x = initSpeed * currentLifeTime * Mathf.Cos(initAngle);
-            float y = initSpeed * currentLifeTime * Mathf.Sin(initAngle)
-                + Physics.gravity.y * Mathf.Pow(currentLifeTime, 2) / 2;
+        float v1 = x * x * g;
+        float v2 = x * Mathf.Tan(initAngle) - y;
+        float v3 = 2 * Mathf.Pow(Mathf.Cos(initAngle), 2);
 
-            transform.position = firePoint.position + new Vector3(x, y, 0); 
+        v0 = Mathf.Sqrt(v1 / (v2 * v3));
+        time = x / (v0 * Mathf.Cos(initAngle));
+    }
 
-            currentLifeTime += Time.deltaTime;
+    private void DrawPath(float initSpeed, float angleRad, float lineStep, float time) {
+        int count = 0;
+
+        lineStep = Mathf.Max(0.01f, lineStep);
+        lineRenderer.positionCount = (int)(time / lineStep) + 2;
+        
+        for(float predictTime = 0; predictTime < time; predictTime+= lineStep) {
+
+            lineRenderer.SetPosition(count, startPos + CalculatePosition(initSpeed, angleRad, predictTime));
+
+            count++;
         }
+
+        lineRenderer.SetPosition(count, startPos + CalculatePosition(initSpeed, angleRad, time));
+    }
+
+    private float QuadraticEquation(float a, float b, float c, float sigh) {
+        return (-b + sigh * Mathf.Sqrt(b * b - 4 * a * c) / (2 * a));
+    }
+
+    private void CalculatePathWithHeight(Vector3 targetPos, float h, out float v0, out float angle, out float time) {
+        float x = targetPos.x;
+        float y = targetPos.y;
+        float g = -Physics.gravity.y;
+
+        float a = (-0.5f * g);
+        float b = Mathf.Sqrt(2 * g * h);
+        float c = -y;
+
+        float tplus = QuadraticEquation(a, b, c, 1);
+        float tmin = QuadraticEquation(-a, b, c, -1);
+
+        time = tplus > tmin ? tplus : tmin;
+        angle = Mathf.Atan(b * time / x);
+        v0 = b / Mathf.Sin(angle);
+    }
+
+    private void MovementHandler(float initSpeed, float angleRad, float lifeTime) {
+        if (currentLifeTime < lifeTime) {
+            transform.position = startPos + CalculatePosition(initSpeed, angleRad, currentLifeTime);
+            currentLifeTime += Time.fixedDeltaTime;
+        }
+    }
+    
+    public Vector3 CalculatePosition(float initSpeed, float angleRad, float time) {
+        float x = initSpeed * time * Mathf.Cos(angleRad);
+        float y = initSpeed * time * Mathf.Sin(angleRad)
+            + Physics.gravity.y * Mathf.Pow(time, 2) / 2;
+
+        Debug.Log(initSpeed);
+
+        return new Vector3(x, y, 0);
     }
 }
 
@@ -65,4 +134,6 @@ public class ProjectileController : MonoBehaviour {
 
 // x = v0 * t * cosA
 // y = v0 * t * sinA - (g * t) / 2
-// 
+
+// Если дойду до реализации отскока, можно активно чекать коллизию по текущему вектору движения с помощью
+// Physics2D(Для текущего прототипа Physics2D).RayCast - одиночным лучом, SphereCast тоже позволит такое сделать, но нужно будет отпределять в каком именно месте сфера получила удар и там смотреть вектор
